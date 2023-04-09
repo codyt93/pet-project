37bc621b43dd667260c46552080bf44d3760713a
codyt93 <128422597+codyt93@users.noreply.github.com>
2023-04-09 10:08:48 -0700

    update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+var urlBreeds = "https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=h4wf8DBUeKwOsqWiLWoU7VN06DEPb7llyE8Sk3Stb1Kyr5ecda&client_secret=KMjhTXuUMtsH7IsUq6eKKcWUkTGBA5JbrHCgil4R"
+var apiKey = "h4wf8DBUeKwOsqWiLWoU7VN06DEPb7llyE8Sk3Stb1Kyr5ecda"
+var secret = "KMjhTXuUMtsH7IsUq6eKKcWUkTGBA5JbrHCgil4R"
+
 fetch("https://api.petfinder.com/v2/oauth2/token", {
     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
     headers: {
