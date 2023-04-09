0608277c9b1503f473cf69674b038f8d10674c3c
MissHeather <heather.hawkes808@gmail.com>
2023-04-09 16:15:20 -0700

    began location

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var urlBreeds = "https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=h4wf8DBUeKwOsqWiLWoU7VN06DEPb7llyE8Sk3Stb1Kyr5ecda&client_secret=KMjhTXuUMtsH7IsUq6eKKcWUkTGBA5JbrHCgil4R"
 var apiKey = "h4wf8DBUeKwOsqWiLWoU7VN06DEPb7llyE8Sk3Stb1Kyr5ecda"
 var secret = "KMjhTXuUMtsH7IsUq6eKKcWUkTGBA5JbrHCgil4R"
+var petDistance = document.getElementById ("petDistance")
+var enterLocation = document.getElementById ("locale")
+var token;
 
 fetch("https://api.petfinder.com/v2/oauth2/token", {
     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
@@ -12,7 +15,9 @@ fetch("https://api.petfinder.com/v2/oauth2/token", {
 .then(res => res.json())
 .then(data => {
   console.log(data)
+  token=data.access_token
   fetchDog(data.access_token)
+  fetchDogLocation(data.access_token,"98258")
 })
 function fetchDog(token){
   fetch("https://api.petfinder.com/v2/animals?type=dog",{
@@ -24,3 +29,18 @@ function fetchDog(token){
     console.log(data)
   })
 }
+function fetchDogLocation(token, zip){
+    fetch("https://api.petfinder.com/v2/animals?location=" + zip,{
+      headers:{
+        Authorization:`Bearer ${token}`
+      }
+    }).then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }
+
+  petDistance.addEventListener("click", function (event){
+    event.preventDefault ()
+    fetchDogLocation(token, enterLocation.value)
+  })
