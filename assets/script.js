f096c632b3318fc00840d1534480798fbd0fc5f0
[Nastasia Sisco] <nsisco@uw.edu>
2023-04-10 19:07:53 -0700

    senior dogs and cats, special needs dogs and cats all have photos now

--- assets/script.js
+++ assets/script.js
@@ -4,34 +4,7 @@ var petDistance = document.getElementById ("petDistance")
 var enterLocation = document.getElementById ("locale")
 var token;
 
-// fetch("https://api.petfinder.com/v2/oauth2/token", {
-//     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
-//     headers: {
-//         "Content-Type": "application/x-www-form-urlencoded"
-//     },
-//     method: "POST"
-// })
-// .then(res => res.json())
-// .then(data => {
-//   console.log(data)
-//   token=data.access_token
-//   fetchDog(data.access_token)
-//   fetchDogLocation(data.access_token,"98258")
-// })
 
-
-
-function fetchDog(){
-  fetch("https://api.petfinder.com/v2/animals?type=dog",{
-    headers:{
-      Authorization:`Bearer ${token}`
-    }
-  }).then(res => res.json())
-  .then(data => {
-    // create elements, append to page
-    console.log(data)
-  })
-}
 
 
 
@@ -57,25 +30,32 @@ function fetchDogLocation(zip){
 
 
 
-
-
-
-//   fetch("https://api.petfinder.com/v2/oauth2/token", {
-//     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
-//     headers: {
-//         "Content-Type": "application/x-www-form-urlencoded"
-//     },
-//     method: "POST"
-// })
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//   fetchSeniorDog(data.access_token)
-// })
-
-
 var petInfoTable = document.getElementById("table");
+
+
+
+function retrieveToken()  {
+  return fetch("https://api.petfinder.com/v2/oauth2/token", {
+    body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "POST"
+})
+.then(res => res.json())
+.then(function (data) {
+  // this where we access the token
+  token=data.access_token
+  // fetchSeniorDog();
+  // fetchSeniorCat();
+  fetchSpecialNeedsCat();
+  // fetchSpecialNeedsDog();
+})
+}
+
+retrieveToken();
+
+
 
 
 function fetchSeniorDog() {
@@ -97,8 +77,6 @@ function fetchSeniorDog() {
 
           petName.textContent = data.animals[i].name;
           petBio.textContent = data.animals[i].description;
-          // how get images to show? It's in an array
-          // Image.src = data.animals[i].photos[0].medium;
           petPic.setAttribute("src", data.animals[i].primary_photo_cropped.medium);
 
           petInfoTable.appendChild(petName);
@@ -117,26 +95,145 @@ function fetchSeniorDog() {
 
 
 
+function fetchSeniorCat() {
+  fetch("https://api.petfinder.com/v2/animals?type=cat&sort=random&age=senior",{
+    headers:{
+      Authorization:`Bearer ${token}`
+    }
+  }).then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var i = 0; i < data.animals.length; i++) {
+
+          var returnedData = data.animals[i];
+          var petPic = document.createElement("img")
+          var petName = document.createElement("h3");
+          var petBio = document.createElement("p");
 
 
-  function retrieveToken()  {
-    return fetch("https://api.petfinder.com/v2/oauth2/token", {
-      body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
-      headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-      },
-      method: "POST"
-  })
-  .then(res => res.json())
-  .then(function (data) {
-    // this where we access to token
-    token=data.access_token
-    fetchSeniorDog()
-  })
-  }
+          petName.textContent = data.animals[i].name;
+          petBio.textContent = data.animals[i].description;
+          petPic.setAttribute("src", data.animals[i].primary_photo_cropped.medium);
 
-  retrieveToken()
-  
+          petInfoTable.appendChild(petName);
+          petInfoTable.appendChild(petPic);
+          petInfoTable.appendChild(petBio);
+                          // how can I omit bios that have special characters?
+
+                          // how can I omit names that have too many characters?
+
+          console.log(returnedData);
+        }
+      })
+    }
+
+
+
+
+function fetchSpecialNeedsCat() {
+  fetch("https://api.petfinder.com/v2/animals?type=cat&sort=random&special_needs=1",{
+    headers:{
+      Authorization:`Bearer ${token}`
+    }
+  }).then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var i = 0; i < data.animals.length; i++) {
+
+          var returnedData = data.animals[i];
+          var petPic = document.createElement("img")
+          var petName = document.createElement("h3");
+          var petBio = document.createElement("p");
+
+
+          petName.textContent = data.animals[i].name;
+          petBio.textContent = data.animals[i].description;
+          petPic.setAttribute("src", data.animals[i].primary_photo_cropped.medium);
+
+          petInfoTable.appendChild(petName);
+          petInfoTable.appendChild(petPic);
+          petInfoTable.appendChild(petBio);
+                          // how can I omit bios that have special characters?
+
+                          // how can I omit names that have too many characters?
+
+          console.log(returnedData);
+        }
+      })
+    }
+
+
+
+
+function fetchSpecialNeedsDog() {
+  fetch("https://api.petfinder.com/v2/animals?type=dog&sort=random&special_needs=1",{
+    headers:{
+      Authorization:`Bearer ${token}`
+    }
+  }).then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        for (var i = 0; i < data.animals.length; i++) {
+
+          var returnedData = data.animals[i];
+          var petPic = document.createElement("img")
+          var petName = document.createElement("h3");
+          var petBio = document.createElement("p");
+
+
+          petName.textContent = data.animals[i].name;
+          petBio.textContent = data.animals[i].description;
+          petPic.setAttribute("src", data.animals[i].primary_photo_cropped.medium);
+
+          petInfoTable.appendChild(petName);
+          petInfoTable.appendChild(petPic);
+          petInfoTable.appendChild(petBio);
+                          // how can I omit bios that have special characters?
+
+                          // how can I omit names that have too many characters?
+
+          console.log(returnedData);
+        }
+      })
+    }
+
+
+
+
     // var token = getToken();
     // console.log(token);
     // fetchSeniorDog(token);
+
+
+
+    // function fetchDog(){
+    //   fetch("https://api.petfinder.com/v2/animals?type=dog",{
+    //     headers:{
+    //       Authorization:`Bearer ${token}`
+    //     }
+    //   }).then(res => res.json())
+    //   .then(data => {
+    //     // create elements, append to page
+    //     console.log(data)
+    //   })
+    // }
+    
+
+// fetch("https://api.petfinder.com/v2/oauth2/token", {
+//     body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${secret}`,
+//     headers: {
+//         "Content-Type": "application/x-www-form-urlencoded"
+//     },
+//     method: "POST"
+// })
+// .then(res => res.json())
+// .then(data => {
+//   console.log(data)
+//   token=data.access_token
+//   fetchDog(data.access_token)
+//   fetchDogLocation(data.access_token,"98258")
+// })
+
